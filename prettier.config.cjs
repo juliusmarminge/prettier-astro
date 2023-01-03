c34a2cd1877ce0d0b4ce71de19b77cbb45575b23
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-03 15:11:34 +0100

    rm excessive config opts

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,12 +1,5 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  arrowParens: "always",
-  printWidth: 80,
-  singleQuote: false,
-  jsxSingleQuote: false,
-  semi: true,
-  trailingComma: "all",
-  tabWidth: 2,
   plugins: [
     require.resolve("prettier-plugin-astro"),
     require.resolve("prettier-plugin-tailwindcss"),
